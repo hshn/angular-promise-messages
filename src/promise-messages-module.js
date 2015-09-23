afa0871cefaab2ae5fdcd396121674997cc5c0f9
Shota Hoshino <lga0503@gmail.com>
2015-09-24 00:24:15 +0900

    Added scheduler

--- src/promise-messages-module.js
+++ src/promise-messages-module.js
@@ -2,10 +2,12 @@ import angular from 'angular';
 import {PromiseMessagesDirective, PromiseMessagesController} from './directives/promise-messages';
 import {PromiseMessageDirective} from './directives/promise-message';
 import {PromiseMessagesProvider} from './providers/promise-messages';
+import {SchedulerFactory} from './services/scheduler';
 
 export default angular
     .module('promiseMessages', [])
     .provider('promiseMessages', PromiseMessagesProvider)
+    .factory('promiseMessagesScheduler', SchedulerFactory)
     .directive('promiseMessages', PromiseMessagesDirective)
     .directive('promiseMessage', PromiseMessageDirective)
     .controller('PromiseMessagesController', PromiseMessagesController)
