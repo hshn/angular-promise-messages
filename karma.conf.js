413860a3d877a00827f387073573d1a64de3e66b
Shota Hoshino <lga0503@gmail.com>
2015-09-23 19:52:45 +0900

    Tweak several configs for test

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'node_modules/angular/angular.js',
             'test/**/*.spec.js'
         ],
 
