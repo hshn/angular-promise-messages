af240a9a403d1ea7813d666f59e37fcbf1b2869f
Shota Hoshino <lga0503@gmail.com>
2015-09-24 21:23:03 +0900

    Bump version v0.3.0

--- dist/angular-promise-messages.js
+++ dist/angular-promise-messages.js
@@ -4,7 +4,7 @@
 Object.defineProperty(exports, '__esModule', {
     value: true
 });
-exports['default'] = PromiseMessageDirective;
+exports.PromiseMessageDirective = PromiseMessageDirective;
 
 function PromiseMessageDirective() {
     var guard = function guard(test, next) {
@@ -22,19 +22,19 @@ function PromiseMessageDirective() {
                 test: function test(state) {
                     return state === when;
                 },
-                attach: function attach(_) {
-                    return guard(function (_) {
+                attach: function attach() {
+                    return guard(function () {
                         return !current;
-                    }, function (_) {
+                    }, function () {
                         transclude(scope, function (cloned) {
                             element.parent().append(current = cloned);
                         });
                     });
                 },
-                detach: function detach(_) {
-                    return guard(function (_) {
+                detach: function detach() {
+                    return guard(function () {
                         return current;
-                    }, function (_) {
+                    }, function () {
                         current.remove();
                         current = null;
                     });
@@ -49,33 +49,7 @@ function PromiseMessageDirective() {
     };
 }
 
-module.exports = exports['default'];
-
 },{}],2:[function(require,module,exports){
-(function (global){
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-    value: true
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-var _angular = (typeof window !== "undefined" ? window['angular'] : typeof global !== "undefined" ? global['angular'] : null);
-
-var _angular2 = _interopRequireDefault(_angular);
-
-var _promiseMessages = require('./promise-messages');
-
-var _promiseMessage = require('./promise-message');
-
-var _promiseMessage2 = _interopRequireDefault(_promiseMessage);
-
-exports['default'] = _angular2['default'].module('promiseMessages', []).directive('promiseMessages', _promiseMessages.PromiseMessagesDirective).directive('promiseMessage', _promiseMessage2['default']).controller('PromiseMessagesController', _promiseMessages.PromiseMessagesController);
-module.exports = exports['default'];
-
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./promise-message":1,"./promise-messages":3}],3:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -95,9 +69,24 @@ var STATE_REJECTED = 'rejected';
 var STATES = [STATE_NONE, STATE_PENDING, STATE_FULFILLED, STATE_REJECTED];
 
 var PromiseMessagesController = (function () {
-    function PromiseMessagesController() {
+    _createClass(PromiseMessagesController, null, [{
+        key: '$inject',
+
+        // TODO: fixme
+        get: function get() {
+            return ['promiseMessages', 'promiseMessagesScheduler'];
+        }
+    }]);
+
+    function PromiseMessagesController(config, scheduler) {
+        var _this = this;
+
         _classCallCheck(this, PromiseMessagesController);
 
+        this.schedule = scheduler(function () {
+            return _this.resetState();
+        });
+        this.config = config;
         this.controls = [];
         this.$state = {};
     }
@@ -130,12 +119,33 @@ var PromiseMessagesController = (function () {
     }, {
         key: 'setState',
         value: function setState(state) {
-            var _this = this;
+            var _this2 = this;
 
             this.$state.name = state;
             STATES.forEach(function (state) {
-                return _this.$state[state] = _this.$state.name === state;
+                return _this2.$state[state] = _this2.$state.name === state;
             });
+
+            if (state === STATE_FULFILLED || state === STATE_REJECTED) {
+                this.tryScheduleResetState();
+            }
+        }
+    }, {
+        key: 'resetState',
+        value: function resetState() {
+            this.setState(STATE_NONE);
+        }
+    }, {
+        key: 'tryScheduleResetState',
+        value: function tryScheduleResetState() {
+            if (this.config.willAutoReset()) {
+                this.scheduleResetState(this.config.getAutoResetAfter());
+            }
+        }
+    }, {
+        key: 'scheduleResetState',
+        value: function scheduleResetState(delay) {
+            this.schedule(delay);
         }
     }]);
 
@@ -196,4 +206,117 @@ function PromiseMessagesDirective($parse, $q) {
     };
 }
 
-},{}]},{},[2]);
+},{}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _angular = require('angular');
+
+var _angular2 = _interopRequireDefault(_angular);
+
+var _directivesPromiseMessages = require('./directives/promise-messages');
+
+var _directivesPromiseMessage = require('./directives/promise-message');
+
+var _providersPromiseMessages = require('./providers/promise-messages');
+
+var _servicesScheduler = require('./services/scheduler');
+
+exports['default'] = _angular2['default'].module('promiseMessages', []).provider('promiseMessages', _providersPromiseMessages.PromiseMessagesProvider).factory('promiseMessagesScheduler', _servicesScheduler.SchedulerFactory).directive('promiseMessages', _directivesPromiseMessages.PromiseMessagesDirective).directive('promiseMessage', _directivesPromiseMessage.PromiseMessageDirective).controller('PromiseMessagesController', _directivesPromiseMessages.PromiseMessagesController);
+module.exports = exports['default'];
+
+},{"./directives/promise-message":1,"./directives/promise-messages":2,"./providers/promise-messages":4,"./services/scheduler":5,"angular":undefined}],4:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+exports.PromiseMessagesProvider = PromiseMessagesProvider;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var PromiseMessagesConfig = (function () {
+    function PromiseMessagesConfig(autoResetAfter) {
+        _classCallCheck(this, PromiseMessagesConfig);
+
+        this.autoResetAfter = autoResetAfter;
+    }
+
+    _createClass(PromiseMessagesConfig, [{
+        key: "getAutoResetAfter",
+        value: function getAutoResetAfter() {
+            return this.autoResetAfter;
+        }
+    }, {
+        key: "willAutoReset",
+        value: function willAutoReset() {
+            return this.autoResetAfter >= 0;
+        }
+    }]);
+
+    return PromiseMessagesConfig;
+})();
+
+function PromiseMessagesProvider() {
+    var _this = this;
+
+    var autoResetAfter = -1;
+
+    this.disableAutoReset = function () {
+        return _this.setAutoResetAfter(-1);
+    };
+    this.setAutoResetAfter = function (ms) {
+        return autoResetAfter = ms;
+    };
+
+    this.$get = function () {
+        return new PromiseMessagesConfig(autoResetAfter);
+    };
+}
+
+},{}],5:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+exports.SchedulerFactory = SchedulerFactory;
+function scheduler($timeout, fn) {
+    var timer = undefined;
+
+    function clearTimer() {
+        if (timer) {
+            $timeout.cancel(timer);
+            timer = null;
+        }
+    }
+
+    function scheduleFn(delay) {
+        if (delay >= 0) {
+            timer = $timeout(fn, delay);
+        }
+    }
+
+    return function schedule(delay) {
+        clearTimer();
+        scheduleFn(delay);
+    };
+}
+
+SchedulerFactory.$inject = ['$timeout'];
+
+function SchedulerFactory($timeout) {
+    return function (fn) {
+        return scheduler($timeout, fn);
+    };
+}
+
+},{}]},{},[3]);
